d602912e4fb59cec5cb253c70ccb1de34dee00c2
Wrtgvr <saldaniil123@gmail.com>
2025-01-05 22:18:13 +0700

    Changable modes
    Now you can change timer mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ export default function App() {
   const defaultPomodoro = 25 
   const [timerSeconds, setTimerSeconds] = useState(defaultPomodoro * 60) // Current time on the timer
   const [paused, setPaused] = useState(true)
-  const [started, setStarted] = useState(false)
   const [settingsOpened, setSettingsOpened] = useState(false)
   const [mode, setMode] = useState("pomodoro")
 
   const [pomodoroTime, setPomodoroTime] = useState(defaultPomodoro * 60) // Work time - minutes * 60
   const [breakTime, setBreakTime] = useState(5 * 60)
   const [longBreakTime, setLongBreakTime] = useState(15 * 60)
+
+  const timeUpdaters = {
+    pomodoro: setPomodoroTime,
+    break: setBreakTime,
+    longBreak: setLongBreakTime,
+  };
 
   useEffect(() => {
     if (!paused) {
@@ -21,51 +26,61 @@ export default function App() {
       }, 1000)
       return () => clearInterval(interval)
     }
-  }, [paused])
+  })
 
-  function convertSeconds(totalSeconds) {
+  useEffect(() => {
+    resetTimer(mode);
+  }, [mode]);
+
+  function convertSecondsToTimer(totalSeconds) {
     const minutes = Math.floor(totalSeconds / 60)
     const seconds = totalSeconds % 60
     return `${minutes}:${seconds.toString().padStart(2, "0")}`
   }
 
   function settingsTimeUpdate(type, newVal_Seconds) {
-    switch (type) {
-      case "pomodoro":
-        setPomodoroTime(newVal_Seconds)
-        break
-      case "break":
-        setBreakTime(newVal_Seconds)
-        break
-      case "longBreak":
-        setLongBreakTime(newVal_Seconds)
-        break
+    if (timeUpdaters[type]) {
+      timeUpdaters[type](newValSeconds);
     }
+  }
+
+  function resetTimer(modeOverride) {
+    const activeMode = modeOverride || mode
+    
+    const time = {
+      pomodoro: pomodoroTime,
+      break: breakTime,
+      longBreak: longBreakTime,
+    }[activeMode];
+
+    setPaused(true)
+    setTimerSeconds(time) 
   }
 
   return (
     <main>
       <div id="timer-container">
         <div id="timer-modes-wrapper">
-          <button onClick={() => setMode("pomodoro")}>Pomodoro</button>
-          <button onClick={() => setMode("break")}>Break</button>
-          <button onClick={() => setMode("longBreak")}>Long break</button>
+          {["pomodoro", "break", "longBreak"].map(type => (
+            <button
+              key={type}
+              className={mode === type ? "chosen-mode-btn" : ""}
+              onClick={() => setMode(type)}>
+              {type.charAt(0).toUpperCase() + type.slice(1)}
+            </button>
+          ))}
         </div>
 
         <div id="timer-wrapper">
-          <span>{convertSeconds(timerSeconds)}</span><br/>
+          <span>{convertSecondsToTimer(timerSeconds)}</span><br/>
         </div>
 
         <div id="timer-controls">
           <button onClick={() => {
               setPaused(prevState => !prevState)
               setStarted(true)
-            }}> {!paused ? "Pause" : started ? "Resume" : "Start"} </button>
-            <button onClick={() => {
-              setPaused(true)
-              setStarted(false)
-              setTimerSeconds(pomodoroTime)
-            }}> Reset </button>
+            }}> {!paused ? "Pause" : "Resume"} </button>
+            <button onClick={() => resetTimer()}> Reset </button>
             <button onClick={() => setSettingsOpened(prevVal => !prevVal)}>
               Settings
             </button>
@@ -77,7 +92,8 @@ export default function App() {
             times={[pomodoroTime / 60, breakTime / 60, longBreakTime / 60]} 
             updateTime={settingsTimeUpdate} 
             closeSettings={() => setSettingsOpened(false)}/>
-       </div>}
+        </div>
+      }
     </main>
   )
 }
